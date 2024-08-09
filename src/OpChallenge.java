87f3568b0deb8f50c4b0804ae30fd2bc67e400e4
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-09 13:49:21 -0400

    la

--- src/OpChallenge.java
+++ src/OpChallenge.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class OpChallenge {
+public class OpChallenge {
+    public static void main(String[] args) {
+        double var1 = 20.00;
+        double var2 = 80.00;
+        double var3 = (var1 + var2) * 100.00;
+
+
+
+    }
 }
